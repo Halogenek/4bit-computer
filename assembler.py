d0101c7f996ff0a858a1496fb1fa0fbb2caa53f4
halogenek <jakubniemczuk@gmail.com>
2016-11-27 00:11:24 +0100

    First working version of the assembler. Next step adding more commands to the CPU.

--- assembler.py
+++ assembler.py
@@ -35,7 +35,9 @@
 #Every word that's not an instruction fron the list abowe will be ignored
 #
 #You can put labels in code with '&LABEL'
-#Then you can direct jump to those labels example:
+#Then you can direct jump to those labels
+#Instructions below the label will be executed
+#example:
 #&LABEL
 #DJMP &LABEL
 ###############################################
@@ -50,112 +52,98 @@ elif len(sys.argv) > 3:
     print('use: program [input_file] [output_file]')
     sys.exit(1)
 machine_code_fp = open(sys.argv[2], 'w')
-machine_code_fp.write('v2.0 raw\r\n')
 labels = {}
-machine_code_line = 0
-with open(sys.argv[1]) as asm_fp:
-    for line in asm_fp:
-        if line[0] == '#':
+machine_code = []
+machine_code.append('v2.0 raw')
+asm_fp = open(sys.argv[1], 'r')
+lines_list = asm_fp.readlines()
+for line in lines_list:
+    if line[0] == '#':
+        continue
+    elif line[0] == '&':
+        labels[line.split()[0]] = len(machine_code) + 1
+    else:
+        if 'NOP' in line:
+            machine_code.append('00')
             continue
-        elif line[0] == '&':
-            labels[line.partition('&')[2].partition(' ')[0]] \
-                = machine_code_fp.tell() - 1
-        else:
-            if 'NOP' in line:
-                machine_code_fp.write('00\r\n')
-                machine_code_line += 1
+        elif 'TOG ' in line:
+            if 'ROM' in line:
+                machine_code.append('10')
                 continue
-            elif 'TOG ' in line:
-                if 'ROM' in line:
-                    machine_code_fp.write('10\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'ALURES' in line:
-                    machine_code_fp.write('11\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'RAMWR' in line:
-                    machine_code_fp.write('12\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'RSEL' in line:
-                    machine_code_fp.write('13\r\n')
-                    machine_code_line += 1
-                    continue
-                else:
-                    sys.exit('unknown TOG argument in line ' \
-                        + str(asm_fp.tell()) + ': ' + line)
-            elif 'WRI ' in line:
-                if 'PCOU' in line:
-                    machine_code_fp.write('20\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PARA' in line:
-                    machine_code_fp.write('21\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PARB' in line:
-                    machine_code_fp.write('22\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PARC' in line:
-                    machine_code_fp.write('23\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'ALUAR' in line:
-                    machine_code_fp.write('24\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'ALUBR' in line:
-                    machine_code_fp.write('25\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'ALUOR' in line:
-                    machine_code_fp.write('26\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'ALURR' in line:
-                    machine_code_fp.write('27\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'RARADDR' in line:
-                    machine_code_fp.write('28\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PCOUZ' in line:
-                    machine_code_fp.write('29\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PCOUNZ' in line:
-                    machine_code_fp.write('2a\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PCOUC' in line:
-                    machine_code_fp.write('2b\r\n')
-                    machine_code_line += 1
-                    continue
-                elif 'PCOUNZ' in line:
-                    machine_code_fp.write('2c\r\n')
-                    machine_code_line += 1
-                    continue
-                else:
-                    sys.exit('unknown WRI argument in line ' \
-                        + str(asm_fp.tell()) + ': ' + line)
-            elif 'WRIDAT ' in line:
-                temp_str = line.split()
-                machine_code_fp.write('3'+temp_str[1]+'\r\n')
-                machine_code_line += 1
+            elif 'ALURES' in line:
+                machine_code.append('11')
                 continue
-            elif 'DJMP ' in line:
-                if '&' in line:
-                    machine_code_fp.write('4' + line.slit()[1])
-                    machine_code_line += 2
-                    continue
-                else:
-                    temp_str = line.split()
-                    temp_str1 = list(temp_str[1])
-                    machine_code_fp.write('4'+temp_str1[0]+'\r\n')
-                    machine_code_fp.write(temp_str1[1]+temp_str1[2]+'\r\n')
-                    machine_code_line += 2
-                    continue
-#TODO calculate jump addresses and write them in the machine code file
+            elif 'RAMWR' in line:
+                machine_code.append('12')
+                continue
+            elif 'RSEL' in line:
+                machine_code.append('13')
+                continue
+            else:
+                sys.exit('unknown TOG argument in line ' \
+                    + lines_list.index(line) + ': ' + line)
+        elif 'WRI ' in line:
+            if 'PCOU' in line:
+                machine_code.append('20')
+                continue
+            elif 'PARA' in line:
+                machine_code.append('21')
+                continue
+            elif 'PARB' in line:
+                machine_code.append('22')
+                continue
+            elif 'PARC' in line:
+                machine_code.append('23')
+                continue
+            elif 'ALUAR' in line:
+                machine_code.append('24')
+                continue
+            elif 'ALUBR' in line:
+                machine_code.append('25')
+                continue
+            elif 'ALUOR' in line:
+                machine_code.append('26')
+                continue
+            elif 'ALURR' in line:
+                machine_code.append('27')
+                continue
+            elif 'RARADDR' in line:
+                machine_code.append('28')
+                continue
+            elif 'PCOUZ' in line:
+                machine_code.append('29')
+                continue
+            elif 'PCOUNZ' in line:
+                machine_code.append('2a')
+                continue
+            elif 'PCOUC' in line:
+                machine_code.append('2b')
+                continue
+            elif 'PCOUNZ' in line:
+                machine_code.append('2c')
+                continue
+            else:
+                sys.exit('unknown WRI argument in line ' \
+                    + lines_list.index(line) + ': ' + line)
+        elif 'WRIDAT ' in line:
+            machine_code.append('3' + line.split() + '')
+            continue
+        elif 'DJMP ' in line:
+            if '&' in line:
+                machine_code.append('4' + ' ' + line.split()[1])
+                machine_code.append('place_holder')
+                continue
+            else:
+                machine_code.append('4' + list(line.split()[1])[0])
+                machine_code.append(list(line.split()[1])[1] + \
+                    list(line.split()[1])[2])
+                continue
+for command in machine_code:
+    if '&' in command:
+        hexaddress_str = "%0.3X" % labels[command.split()[1]]
+        index_of_label = machine_code.index(command)
+        machine_code[index_of_label] = command[0] \
+            + hexaddress_str[0]
+        machine_code[index_of_label + 1] = hexaddress_str[1:3]
+for line in machine_code:
+    machine_code_fp.write(line + '\r\n')
